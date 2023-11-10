c33b14c74203779aab5cd8528498f466804c632a
Dot1298 <101444537@georgebrown.ca>
2023-11-09 22:42:13 -0500

    Having issues- will try again tomorrow

--- Assets/!Script/Enemy/EnemyMovement.cs
+++ Assets/!Script/Enemy/EnemyMovement.cs
@@ -4,17 +4,20 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _enemyMoveSpeed = 8f;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    private void Awake()
     {
-
+        if (_rigidbody == null)
+            _rigidbody = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 moveDirection = _enemyMoveSpeed * Time.fixedDeltaTime * new Vector3(-1f, 0, 0).normalized;
+        _rigidbody.AddForce(moveDirection, ForceMode2D.Impulse);
+
     }
 }
--- Assets/!Script/Enemy/EnemySpawner.cs
+++ Assets/!Script/Enemy/EnemySpawner.cs
@@ -6,29 +6,10 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
-    [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private float _enemyMoveSpeed = 8f;
-    private bool hasSpawned = false;
 
-    private void Awake()
-    {
-        if (_rigidbody == null)
-            _rigidbody = GetComponent<Rigidbody2D>();
-    }
     private void Start()
-    {
-        if (!hasSpawned)
-        {
+    {    
             GameObject enemy = Instantiate(_enemyPrefab, new Vector3(Random.Range(20, 60), 0, 0), Quaternion.identity);
-            hasSpawned = true;
             //GameObject enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
-        }
-
-    }
-    private void FixedUpdate()
-    {
-        Vector3 moveDirection = _enemyMoveSpeed * Time.fixedDeltaTime * new Vector3(-1f, 0, 0).normalized;
-        _rigidbody.AddForce(moveDirection, ForceMode2D.Impulse);
-
     }
 }
